19482d9e7191be96c62dfa8c8a74650667d3ab6e
Andres Masis <67446603+AndresMasis@users.noreply.github.com>
2021-07-12 17:11:36 -0600

    Teacher name updates
    You can update the name of a course, and update the name of a teacher in a single course or an all courses

--- scheduleLogic.py
+++ scheduleLogic.py
@@ -45,6 +45,7 @@ def addTeacherToCourse(courseName, teacherName, teacherSchedule):
    It receives as parameter a string that is the course name (the key of the course)
    It does not return anything'''
 
+
 def deleteCourse(courseName):
     del coursesDictionary[courseName]  # Deletes the course in the dictionary
     keysList.remove(courseName)  # Keeps the order for the keys backup
@@ -55,8 +56,63 @@ def deleteCourse(courseName):
    It receives as parameter a string that is the key of the course where the teacher resides
    another string that is the name of the new teacher (its key in the teachers dictionary)
    It does not return anything'''
-def addTeacherFromCourse(courseName, teacherName):
+
+
+def deleteTeacherFromCourse(courseName, teacherName):
     del coursesDictionary[courseName][0][teacherName]
+
+
+'''-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+   This function changes the name of a course
+   This will change the key of the course in coursesDictionary
+   Also rearranges the keysList
+   It receives as parameter a string that is the current key of the course in coursesDictionary
+   another string that is the new name of the course (it will be the new key in coursesDictionary)
+   It does not return anything'''
+
+
+def updateCourseName(oldName, newName):
+    # Updates the dictionary
+    coursesDictionary[newName] = coursesDictionary.pop(oldName)
+
+    # Updates the list
+    keysList.remove(oldName)
+    keysList.append(newName)
+
+
+'''-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+   This function changes the name of a teacher in a specific course
+   This will change the key of the teacher in the teachers dictionary of just that course
+   It receives as parameter a string that is the key of the course where the teacher currently resides
+   another string that is the current (name) key of the teacher in the teachers dictionary of that course
+   also a string that is the new name of the teacher (it will be the new key in the teacher dictionary of that course)
+   It does not return anything'''
+
+
+def updateTeacherNameOnce(courseKey, oldName, newName):
+    # Updates the teachers dictionary of the given course
+    coursesDictionary[courseKey][0][newName] = coursesDictionary[courseKey][0].pop(oldName)
+
+
+'''-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+   This function changes the name of a teacher in all the courses he appears
+   This will change the key of the teacher in the teachers dictionary of all the courses he appears
+   It receives as parameter a string that is the current (name) key of the teacher in the teachers dictionary
+   also a string that is the new name of the teacher (it will be the new key in the teacher dictionary)
+   It does not return anything'''
+
+
+def updateTeacherNameAll(oldName, newName):
+    # Goes thru all the courses dictionary checking course by course
+    for courseKey, courseValue in coursesDictionary.items():
+        # It takes a specific course to analyze
+        # courseValue looks like [{'Jorge': [(0,1),(0,2),(3,1),(3,2)], 'Kirstein': [(5,8),(6,9),(5,8),(6,9)]}, 4]
+
+        # Checks if the teacher (key) exists in the teachersDictionary
+        if oldName in courseValue[0].keys():
+            # That teacher does exist, so it can be modified
+            coursesDictionary[courseKey][0][newName] = coursesDictionary[courseKey][0].pop(oldName)
+
 
 
 '''-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
@@ -113,4 +169,21 @@ def makeCombinations():
             generatedSchedules.append(combination)
 
 
+'''-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+   Main function
+   Currently used for tests'''
 
+
+def main():
+    addCourse("Analisis de Algoritmos", 4)
+    addCourse("Arquitectura de Computadores",4)
+    addCourse("Seminarios de Estudios Filosoficos", 2)
+    addTeacherToCourse("Arquitectura de Computadores", "Jorge Vargas", [(4, 4), (4, 5), (6, 4), (6, 5)])
+    addTeacherToCourse("Arquitectura de Computadores", "Kirstein Gatjens", [(0, 3), (0, 3), (2, 3), (2, 4)])
+    addTeacherToCourse("Seminarios de Estudios Filosoficos", "Kirstein Gatjens", [(0, 3), (0, 3), (2, 3), (2, 4)])
+    addTeacherToCourse("Analisis de Algoritmos", "Kirstein Gatjens", [(0, 3), (0, 3), (2, 3), (2, 4)])
+    updateTeacherNameAll("Kirstein Gatjens", "Esteban Arias")
+
+
+if __name__ == "__main__":
+    main()
